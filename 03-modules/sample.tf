b5e61c32caf94febe534c0674f2f8cd6d9d2d693
Sandeep Martha <sandeepvarma508211@gmail.com>
2022-06-24 09:44:56 +0530

    Merge remote-tracking branch 'origin/main'

---
+++ 03-modules/sample.tf
@@ -0,0 +1,11 @@
+module "ec2" {
+  source = "./ec2"
+  sg_id = module.securitygroup.sg_id
+}
+module "securitygroup" {
+  source = "./securitygroup"
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
